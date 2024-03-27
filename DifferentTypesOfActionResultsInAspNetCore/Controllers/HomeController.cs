2a3b893ad90353c0abf300edc50df5aab827f37c
CodeAdmirer <codeadmirer@gmail.com>
2024-03-28 01:02:07 +0530

    Added initial commit

---
+++ DifferentTypesOfActionResultsInAspNetCore/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using DifferentTypesOfActionResultsInAspNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DifferentTypesOfActionResultsInAspNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Privacy()
+        {
+            return Json(null);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
