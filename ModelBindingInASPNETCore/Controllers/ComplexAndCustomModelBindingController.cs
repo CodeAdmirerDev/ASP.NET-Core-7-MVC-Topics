047b3d8303dc695543913d49536d9a0d81d6e7cb
CodeAdmirer <codeadmirer@gmail.com>
2024-08-06 01:41:51 +0530

    All about Complex view data binding

---
+++ ModelBindingInASPNETCore/Controllers/ComplexAndCustomModelBindingController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ModelBindingInASPNETCore.Models;
+
+namespace ModelBindingInASPNETCore.Controllers
+{
+    public class ComplexAndCustomModelBindingController : Controller
+    {
+        [HttpGet("moviedetails/create")]
+        // [Route("moviedetails/create")] same in attribute routing
+        public ViewResult CreateMovie()
+        {
+            ViewBag.DirectorsList = Enum.GetValues(typeof(Directors)).Cast<Directors>().ToList();
+
+            ViewBag.MovieTypeList = new List<SelectListItem>() {
+
+                new SelectListItem {Text="Please select movie type", Value="0", Selected=true },
+                new SelectListItem {Text="Horror", Value="1" },
+                new SelectListItem {Text="Family", Value="2" },
+                new SelectListItem {Text="Comedy", Value="3"},
+                new SelectListItem {Text="SCFI", Value="4"},
+
+             };
+
+            ViewBag.CastList = new List<string> { "NTR", "PK", "RAM", "NBK" };
+
+            //To get single value of enum ====> var SSRval = Directors.SSR;
+            return View();
+        }
+
+        [HttpPost("moviedetails/create")]
+        public IActionResult CreateMovie(MovieDetails movieDetails)
+        {
+
+            if (ModelState.IsValid)
+            {
+                TempData["SuccessMsg"] = "Movie created!";
+                return RedirectToAction("SuccessView", "ModelBinding");
+            }
+            else
+            {
+                ViewBag.DirectorsList = Enum.GetValues(typeof(Directors)).Cast<Directors>().ToList();
+                ViewBag.MovieTypeList = new List<SelectListItem>() {
+                new SelectListItem {Text="Please select movie type", Value="0", Selected=true },
+                new SelectListItem {Text="Horror", Value="1" },
+                new SelectListItem {Text="Family", Value="2" },
+                new SelectListItem {Text="Comedy", Value="3"},
+                new SelectListItem {Text="SCFI", Value="4"},
+             };
+
+                ViewBag.CastList = new List<string> { "NTR", "PK", "RAM", "NBK" };
+
+                //To get single value of enum ====> var SSRval = Directors.SSR;
+                return View();
+            }
+        }
+    }
+}
--- ModelBindingInASPNETCore/Models/Movie.cs
+++ ModelBindingInASPNETCore/Models/Movie.cs
@@ -2,11 +2,46 @@
 
 namespace ModelBindingInASPNETCore.Models
 {
+
+    public enum Directors
+    {
+        SSR=1,
+        Poori,
+        Sukumar,
+        ShekarKamula
+
+    }
+
+    public enum MovieType
+    {
+        Horror=1,
+        Family,
+        Comedy,
+        SCFI
+
+    }
+
     public class Movie
     {
+        [Required(ErrorMessage = "Movie Name is required")]
+        public string MovieName { get; set; }
+        public string? HeroName { get; set; }
+        public double? MovieCollection { get; set; }
+    }
+
+    public class MovieDetails
+    {
+        public int? MovieId { get; set; }
+
         [Required(ErrorMessage="Movie Name is required")]
         public string MovieName { get; set; }
         public string? HeroName { get; set; }
         public double? MovieCollection { get; set; }
+        public Directors? Directors { get; set; }
+        public MovieType? MovieType { get; set; }
+        public List<string> Cast { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public bool IsMovieHit { get; set; }
+        public string MovieStory { get; set; }
     }
 }
