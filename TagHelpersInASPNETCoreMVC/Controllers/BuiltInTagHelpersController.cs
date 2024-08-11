5dde33dc08345928e0516cd6279303ddbe96896a
CodeAdmirer <codeadmirer@gmail.com>
2024-08-11 23:10:48 +0530

    All about Tag Helpers in ASP.NET Core MVC

---
+++ TagHelpersInASPNETCoreMVC/Controllers/BuiltInTagHelpersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TagHelpersInASPNETCoreMVC.Models;
+
+namespace TagHelpersInASPNETCoreMVC.Controllers
+{
+    public class BuiltInTagHelpersController : Controller
+    {
+        private List<Product> products;
+
+        public BuiltInTagHelpersController()
+        {
+            products = new List<Product>()
+            {
+                new Product(){ProductId=1,ProductName="Dell Laptop",Description="Inspiron 5559", Category="Laptops",InStock=true,Price=35000},
+                new Product(){ProductId=1,ProductName="HP Laptop",Description="HP 5559", Category="Laptops",InStock=true,Price=55000},
+                new Product(){ProductId=1,ProductName="LG Mobile",Description="LG smart phone", Category="Mobile",InStock=true,Price=15000},
+                new Product(){ProductId=1,ProductName="Samsung Mobile",Description="Samsung smart phone", Category="Mobile",InStock=true,Price=35000},
+            };
+        }
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+        public ViewResult ProductDetailsById(int productid)
+        {
+            var productDetails = products.FirstOrDefault(prd => prd.ProductId == productid);
+
+            return View(productDetails);
+        }
+    }
+}
---
+++ TagHelpersInASPNETCoreMVC/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace TagHelpersInASPNETCoreMVC.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public required string ProductName { get; set; }
+        public double Price { get; set; }
+        public required string Description { get; set; }
+        public string? Category { get; set; }
+        public bool InStock { get; set; }
+    }
+}
