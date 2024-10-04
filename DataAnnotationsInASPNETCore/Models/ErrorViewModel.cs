09b29fb1458176a014ac472addebdfe180088499
CodeAdmirer <codeadmirer@gmail.com>
2024-10-05 00:00:08 +0530

    Added intial code base

---
+++ DataAnnotationsInASPNETCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DataAnnotationsInASPNETCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
